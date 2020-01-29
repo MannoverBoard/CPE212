834539a4d4f5e68403221f9aa6534d04239bb3f5
Patrick <patrickdberry802@gmail.com>
2020-01-29 00:31:51 -0600

    Adding macro black magic & fixing syntax erorrs & toString functions

--- Project2/Project02-Student/stuff.cpp
+++ Project2/Project02-Student/stuff.cpp
@@ -1,48 +1,59 @@
 #include "stuff.h"
+#include <cmath>
 
-const std::string MISSING_TYPE_NAME = "Type";
-const std::map<Race,std::string> type2string{
-	{Race::POTION    , "POTION"   },
-	{Race::WEAPON    , "WEAPON"   },
-	{Race::ARMOR     , "ARMOR"    },
-	{Race::ACCESSORY , "ACCESSORY"}
-};
-
-std::string toString(const Type type) {
-	const auto it = type2string.find(type);
-	if(it==type2string.end()) {
-		return MISSING_TYPE_NAME;
-	} else {
-		return it->second;
-	}
+std::string toString(const Type& type) {
+  switch(type) {
+  case POTION   : return "POTION"   ; break;
+  case WEAPON   : return "WEAPON"   ; break;
+  case ARMOR    : return "ARMOR"    ; break;
+  case ACCESSORY: return "ACCESSORY"; break;
+  }
+  return "INVALID TYPE";
 }
-
-const std::string MISSING_RACE_NAME = "MISSING_NAME";
-const std::map<Race,std::string> race2sring{
-	{Race::HUMAN , RaceStrings[0]},
-	{Race::HUMAN , RaceStrings[1]},
-	{Race::HUMAN , RaceStrings[2]},
-	{Race::HUMAN , RaceStrings[3]},
-	{Race::HUMAN , RaceStrings[4]}
-};
 
 std::string toString(const Race race) {
-	const auto it = race2sring.find(race);
-	if(it==race2sring.end()) {
-		return MISSING_RACE_NAME;
-	} else {
-		return it->second;
-	}
-}
-
-std::string toString(const Item& item) {
-	std::ostringstream oss;
-	oss << item.name << " Value: " << item.value;
-	return oss.str();
+  switch(race) {
+  case HUMAN : return "Human" ; break; 
+  case ELF   : return "Elf"   ; break;
+  case DWARF : return "Dwarf" ; break;
+  case GNOME : return "Gnome" ; break;
+  case GOBLIN: return "Goblin"; break;
+  }
+  return "INVALID RACE";
 }
 
 
 Item toItem(const Weapon& weapon) {
-	return Item{.name = weapon.name,.value = weapon.value,.type = TYPE::WEAPON };
+	return Item{.name = weapon.name,.value = static_cast<decltype(Item::value)>(weapon.cost), .type = WEAPON };
 };
 
+double rounding(const double value,const RoundingEvent event) {
+  //implements game-wide rounding logic
+  double out;
+  int round_rule = -1;
+  switch(event) {
+  case RoundingEvent::Unknown:
+    round_rule = 0;
+    break;
+  case RoundingEvent::Player:
+    round_rule = 1;
+    break;
+  case RoundingEvent::Enemy:
+    round_rule = -1;
+  }
+  switch(ROUNDING_RULE) {
+  case RoundingRule::AlwaysRoundDown:
+    round_rule = -1;
+    break;
+  case RoundingRule::PlayerFavor:
+    break;
+  case RoundingRule::EnemyFavor:
+    round_rule *= -1;
+  }
+  switch(round_rule) {
+  case -1:out = floor(value);break;
+  case  0:out = round(value);break;
+  case +1:out =  ceil(value);break;
+  }
+  return out;
+}
